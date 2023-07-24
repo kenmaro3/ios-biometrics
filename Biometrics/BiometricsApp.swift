918649954431f3ce4fef483fb70eeb3694729f95
kenmaro3 <miha.ken.19@gmail.com>
2023-07-24 16:52:46 +0900

    Initial Commit

---
+++ Biometrics/BiometricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricsApp.swift
+//  Biometrics
+//
+//  Created by Kentaro Mihara on 2023/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BiometricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
